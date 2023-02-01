075566dd5a15d89ea56ac3a37ba9f5f2d0374092
Ali Ramazani <78242283+ramazania@users.noreply.github.com>
2023-01-31 22:27:12 -0600

    Update gpaCalculator.py

--- projects/gpaCalculator.py
+++ projects/gpaCalculator.py
@@ -1,92 +1,27 @@
-gradeScale = {
-  "A" : 4.00,
-  "A-" : 3.67,
-  "B+" : 3.33,
-  "B" : 3.00,
-  "B-" : 2.67,
-  "C+" : 2.33,
-  "C" : 2.00,
-  "C-" : 1.67,
-  "D+" : 1.33,
-  "D" : 1.00,
-  "D-" : .67,
-  "F" : .00
-}
-  
-
-def gradeLetter():
-  grade = int(input("Please enter your grade: "))
-  match grade:
-      case num if num in range (93, 101):
-          return("A")
-      case num if num in range (90, 93):
-          return("A-")
-      case num if num in range (87, 90):
-          return("B+")
-      case num if num in range (83, 87):
-          return("B")
-      case num if num in range (80, 83):
-          return("B-")
-      case num if num in range (77, 80):
-          return("C+")
-      case num if num in range (73, 77):
-          return("C")
-      case num if num in range (70, 73):
-          return("C-")
-      case num if num in range (67, 70):
-          return("D+")
-      case num if num in range (63, 67):
-          return("D")
-      case num if num in range (60, 63):
-          return("D-")
-      case num if num in range (0, 60):
-          return("F")
-      case invalid_grade:
-          return(f"Invalid grade: {invalid_grade}")
-  
+def calculate_gpa(grades):
+    grade_value = {
+        "A": 4.0,
+        "A-": 3.67,
+        "B+": 3.33,
+        "B": 3.0,
+        "B-": 2.67,
+        "C+": 2.33,
+        "C": 2.0,
+        "C-": 1.67,
+        "D+": 1.33,
+        "D": 1.0,
+        "D-": 0.67,
+        "F": 0.0
+    }
     
-def addToLst():
-  lst = []
-  for i in range(3):
-    x =  gradeLetter()
-    lst.append(x)
-  print("This is the list: ", lst)
-  return lst
-            
-
-def gradeSearch(lst):
-  gradeList = []
-  found = False
-  keyList = gradeScale.keys()
-  for element in lst:
-    for key in keyList:
-        if key == element:
-            result = gradeScale.get(key)
-            print("Grade was found in the dictionary: ", result)
-            found = True
-            gradeList.append(result)
-  if found == False:
-      print("Grade was not found in the dictionary")
-  else:
-      return(gradeList)
-      
-
-def gpaCalculator(lst):
-  count = 0
-  sum = 0
-  for i in lst:
-      sum += i
-      count += 1
-  gpa = sum / count
-  return gpa
-                            
-            
-def main():
-    gradeLetterList = addToLst()
-    gradeScaleList = gradeSearch(gradeLetterList)
-    gpa = gpaCalculator(gradeScaleList)
-    print("This is your gpa for the term: ", gpa)
+    total = 0
+    num_grades = 0
     
+    for grade in grades:
+        total += grade_value[grade]
+        num_grades += 1
+        
+    return total / num_grades
 
-if __name__ == "__main__":
-    main()  
+# example usage
+print(calculate_gpa(["B", "A-", "A-"]))
