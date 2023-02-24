a24669419e1c90a1c32cd526a728565cbb59aed0
ramazania <ramazania@DESKTOP-P6F17UN.localdomain>
2023-02-24 15:36:47 -0600

    added my internship tracker python program along with the csv file.

---
+++ projects/internship_tracker.py
@@ -0,0 +1,32 @@
+import csv
+import getpass
+import os
+import time
+
+# Define the path to the CSV file
+CSV_FILE_PATH = "internship_applications.csv"
+
+# Check if the CSV file exists, and create it if it doesn't
+if not os.path.exists(CSV_FILE_PATH):
+    with open(CSV_FILE_PATH, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Job Portal", "E-mail", "Username", "Company", "Date Applied", "Status", "Location", "Job Field"])
+
+# Prompt the user to enter the internship application details
+job_portal = input("Job Portal: ")
+email = input("E-mail: ")
+username = input("Username: ")
+#password = getpass.getpass("Password: ")
+company = input("Company: ")
+date_applied = time.strftime("%Y-%m-%d %H:%M:%S")
+status = input("Status: ")
+location = input("Location: ")
+job_field = input("Job Field: ")
+
+# Write the details to the CSV file
+with open(CSV_FILE_PATH, "a", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow([job_portal, email, username, company, date_applied, status, location, job_field])
+
+# Print a success message
+print("Application saved successfully.")
